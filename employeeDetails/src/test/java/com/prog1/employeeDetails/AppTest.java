9deb17144d77faeacf9adfea644583baf9d9e180
Sunee Patchava <Suneela.Patchava@gmail.com>
2020-07-01 23:42:33 -0500

    default branch

---
+++ employeeDetails/src/test/java/com/prog1/employeeDetails/AppTest.java
@@ -0,0 +1,20 @@
+package com.prog1.employeeDetails;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
